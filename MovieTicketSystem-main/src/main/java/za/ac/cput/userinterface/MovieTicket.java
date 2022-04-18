2acfa6134c8ee230245108330cf80b6b8f14fc16
Chad Siegelaar <218340982@mycput.ac.za>
2022-04-18 15:19:56 +0200

    Completed Ticket entity

---
+++ MovieTicketSystem-main/src/main/java/za/ac/cput/userinterface/MovieTicket.java
@@ -0,0 +1,15 @@
+/* MovieTicket.java
+ User Interface for the MovieTicket
+ Author: Chad Siegelaar (218340982)
+ Date: 16 April 2022
+*/
+
+package za.ac.cput.userinterface;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Scanner;
+
+public class MovieTicket {
+
+}
